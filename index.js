dd37fe41b21f6036234fe31c38433fb5e4cf380f
Md Hasan <mdhasan8064@gmail.com>
2023-01-23 21:04:13 +0600

    admin check & some rest api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
@@ -15,6 +16,21 @@ app.get('/', (req, res) => {
 })
 
 
+const verifyjwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send("protomei error khaiso")
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.USER_JWT, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbiden access" })
+        }
+        req.decoded = decoded;
+        // console.log('supply decoded', req.decoded)
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.SECRET_DB_id}:${process.env.SECRET_DB_pass}@cluster0.dof2kcq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,6 +40,21 @@ const run = async () => {
     const users = client.db('REPLIQ_Task').collection("users");
     const cart = client.db('REPLIQ_Task').collection("Cart");
     const orders = client.db('REPLIQ_Task').collection("Orders");
+
+
+
+    //jwt Token
+    app.get('/jwt', async (req, res) => {
+        const userMail = req.query.email;
+        const query = { email: userMail };
+        const user = await users.findOne(query);
+        if (user) {
+            const token = jwt.sign({ userMail }, process.env.USER_JWT, { expiresIn: '24h' });
+            return res.send({ accessToken: token })
+        }
+        res.status(403).send({ accessToken: "" })
+    })
+
 
     //all Product
     app.get('/products', async (req, res) => {
@@ -41,13 +72,14 @@ const run = async () => {
 
     // All Users 
     app.get('/users', async(req, res) =>{
-        const result = await users.find({title: "user"}).toArray();
+        const result = await users.find({}).toArray();
         res.send(result);
     })
 
     // ALl cart view
-    app.get('/cart', async (req, res) => {
-        const result = await cart.find({}).toArray();
+    app.get('/cart/:email', async (req, res) => {
+        const email = req.params.email;
+        const result = await cart.find({orderBy: email}).toArray();
         res.send(result);
     })
 
@@ -57,6 +89,14 @@ const run = async () => {
         const query = {_id: ObjectId(id)};
         const result = await users.findOne(query);
         res.send(result);
+    })
+
+    // Check Admin
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = {email: email};
+        const result = await users.findOne(query);
+        res.send({isAdmin: result?.type === "Admin"});
     })
 
     // Admin Order List
@@ -87,10 +127,18 @@ const run = async () => {
         res.send(result);
     })
 
+
+    // Add  New Order
+    app.post('/orders', async(req, res) =>{
+        const data = req.body;
+        const result = await orders.insertOne(data);
+        res.send(result);
+    })
+
     // Add new Product 
     app.post('/addproduct', async (req, res) => {
         const data = req.body;
-        const result = await users.insertOne(data);
+        const result = await productsCollection.insertOne(data);
         res.send(result)
     });
 
